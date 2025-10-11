f3a0b3f5769e5df61b5b4db9621a6e3eb5b5fbe9
Stephen Gadd <42514781+docuracy@users.noreply.github.com>
2025-10-11 21:36:44 +0000

    Update documentation: 2025-10-11 21:36:44 -

--- start_preview.sh
+++ start_preview.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-/home/vscode/.local/bin/python -m sphinx_autobuild . _build/html --port 8000 --open-browser
+python -m sphinx_autobuild . _build/html --port 8000 --open-browser
